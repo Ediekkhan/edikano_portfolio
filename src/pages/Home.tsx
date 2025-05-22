b1f89f4a32eb0b3fc39b3b421fcca4e36b6bd8b9
Ediekkhan <eddychristantus@gmail.com>
2025-05-21 19:11:27 -0700

    minor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -71,7 +71,7 @@ export default function Home() {
                   />
                 </div>
                 <div className="absolute -bottom-4 -right-4 bg-white text-gray-900 py-2 px-4 rounded-lg shadow-lg font-medium">
-                  <span className="text-teal-600">+ years</span> experience
+                  <span className="text-teal-600">3+ years</span> experience
                 </div>
               </div>
             </motion.div>
